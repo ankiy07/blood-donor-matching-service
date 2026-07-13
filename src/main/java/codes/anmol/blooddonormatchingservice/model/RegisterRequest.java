77a7158c4082dac6bedd0e6910c902fc5e03104a
ankiy07 <aankitsingh0717@gmail.com>
2026-07-13 18:40:11 +0530

    added models

--- src/main/java/codes/anmol/blooddonormatchingservice/model/RegisterRequest.java
+++ src/main/java/codes/anmol/blooddonormatchingservice/model/RegisterRequest.java
@@ -1,4 +1,12 @@
 package codes.anmol.blooddonormatchingservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
 }
--- src/main/java/codes/anmol/blooddonormatchingservice/model/User.java
+++ src/main/java/codes/anmol/blooddonormatchingservice/model/User.java
@@ -1,4 +1,31 @@
 package codes.anmol.blooddonormatchingservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // stored as a BCrypt hash, never plain text
+
+    @Column(nullable = false)
+    private String name;
 }
