18c90cb892a286ba5ad962fb86980bc84634b71b
ankiy07 <aankitsingh0717@gmail.com>
2026-07-13 18:41:22 +0530

    added repository

--- src/main/java/codes/anmol/blooddonormatchingservice/repository/UserRepository.java
+++ src/main/java/codes/anmol/blooddonormatchingservice/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package codes.anmol.blooddonormatchingservice.repository;
 
-public class UserRepository {
+import codes.anmol.blooddonormatchingservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
